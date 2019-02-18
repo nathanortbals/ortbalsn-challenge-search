41a0fc7ebf99c9b2fa937c2c47ab033735489668
Nathan Ortbals <nathan.ortbals@gmail.coma>
2019-02-18 14:50:01 -0600

    Initial Commit

---
+++ ortbalsn-challenge-core-dataTests/ortbalsn_challenge_core_dataTests.swift
@@ -0,0 +1,34 @@
+//
+//  ortbalsn_challenge_core_dataTests.swift
+//  ortbalsn-challenge-core-dataTests
+//
+//  Created by Nathan Ortbals on 2/18/19.
+//  Copyright © 2019 Nathan Ortbals. All rights reserved.
+//
+
+import XCTest
+@testable import ortbalsn_challenge_core_data
+
+class ortbalsn_challenge_core_dataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
